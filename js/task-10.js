240b4b073dbd5bbac9fb674250bec9c0a719f754
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-01-03 01:49:17 +0300

    continues task 10

--- js/task-10.js
+++ js/task-10.js
@@ -6,22 +6,27 @@ const refs = {
   boxes: document.querySelector('#boxes'),
 };
 
-const newElements = [];
+refs.createBtn.addEventListener('click', createBoxes);
+// refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+// const newElements = [];
+
+refs.number.addEventListener('input', element => {
+  console.log(element.currentTarget.value);
+});
 
 function createBoxes(amount) {
   const elements = document.createElement('div');
-  console.log(elements);
-  newElements.push(elements);
-  // Принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
-  // Размеры самого первого <div> - 30px на 30px.
-  // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-  // Все элементы должены иметь случайный цвет фона в формате HEX.
+  elements.classList.add('div_boxes');
+  elements.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+  refs.boxes.append(elements);
+
+  for (let i = 1; i <= amount; i += 1) {}
 }
 
-refs.boxes.append(...newElements);
+// Принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+// Размеры самого первого <div> - 30px на 30px.
+// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function destroyBoxes() {}
+// function destroyBoxes() {}
