b5e7e25ca1769b0cc1800adae57430f85b130029
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-01-03 23:45:08 +0300

    adds some changes for task 10

--- js/task-10.js
+++ js/task-10.js
@@ -7,26 +7,28 @@ const refs = {
 };
 
 refs.createBtn.addEventListener('click', createBoxes);
-// refs.destroyBtn.addEventListener('click', destroyBoxes);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
 
 // const newElements = [];
 
-refs.number.addEventListener('input', element => {
-  console.log(element.currentTarget.value);
-});
-
 function createBoxes(amount) {
+  refs.number.addEventListener('input', element => {
+    console.log(element.currentTarget.value);
+  });
+
   const elements = document.createElement('div');
   elements.classList.add('div_boxes');
   elements.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
   refs.boxes.append(elements);
 
-  for (let i = 1; i <= amount; i += 1) {}
+  // for (let i = 1; i <= amount; i += 1) {}
 }
 
 // Принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
 // Размеры самого первого <div> - 30px на 30px.
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 
-// function destroyBoxes() {}
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
