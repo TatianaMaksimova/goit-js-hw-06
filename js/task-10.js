a92f86a31dbd81d968d3d099787c54ec3b1b64c2
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-01-06 20:40:15 +0300

    adds some changes for task-10

--- js/task-10.js
+++ js/task-10.js
@@ -9,27 +9,40 @@ const refs = {
 refs.createBtn.addEventListener('click', createBoxes);
 refs.destroyBtn.addEventListener('click', destroyBoxes);
 
-// const newElements = [];
+const newElements = [];
 
-refs.number.addEventListener('input', element => {
-  console.log(element.currentTarget.value);
-});
+// refs.number.addEventListener('input', element => {
+//   console.log(element.currentTarget.value);
+// });
 
 function createBoxes(amount) {
-  refs.number.addEventListener('input', element => {
-    return element.currentTarget.value;
-  });
-
-  amount = element.currentTarget.value;
-
   for (let i = 1; i <= amount; i += 1) {
     const elements = document.createElement('div');
     elements.classList.add('div_boxes');
     elements.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-    refs.boxes.append(elements);
+    newElements.push(elements);
   }
 }
+
+console.log(newElements);
+
+// createBoxes(
+//   refs.number.addEventListener('input', element => {
+//     console.log(element.currentTarget.value);
+//   }),
+// );
+
+createBoxes(11);
+
+// const readyBoxes = createBoxes(
+//   refs.number.addEventListener('input', element => {
+//     console.log(element.currentTarget.value);
+//   }),
+// );
+// console.log(readyBoxes);
+
+refs.boxes.append(...newElements);
 
 // Принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
 // Размеры самого первого <div> - 30px на 30px.
