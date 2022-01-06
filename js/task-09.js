4a3e9226f8a1bf1bc2bd1659611784eb51180884
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-01-06 16:03:39 +0300

    corrects task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,11 @@
 const refs = {
   buttonEl: document.querySelector('.change-color'),
   textEl: document.querySelector('.color'),
-  divEl: document.querySelector('.widget'),
-  // body: document.querySelector('body'),
 };
 
 refs.buttonEl.addEventListener('click', getRandomHexColor);
 
 function getRandomHexColor() {
-  refs.divEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  refs.textEl.textContent = refs.divEl.style.backgroundColor;
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  refs.textEl.textContent = document.body.style.backgroundColor;
 }
