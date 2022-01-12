f4b6ed6d45baffe89ce2249c2149893f29cf1059
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-01-13 00:59:09 +0300

    adds 'for' in task-10

--- js/task-10.js
+++ js/task-10.js
@@ -9,44 +9,23 @@ const refs = {
 refs.createBtn.addEventListener('click', createBoxes);
 refs.destroyBtn.addEventListener('click', destroyBoxes);
 
-const newElements = [];
-
-// refs.number.addEventListener('input', element => {
-//   console.log(element.currentTarget.value);
-// });
-
 function createBoxes(amount) {
-  for (let i = 1; i <= amount; i += 1) {
-    const elements = document.createElement('div');
-    elements.classList.add('div_boxes');
-    elements.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let userNumber = refs.number.value;
 
-    newElements.push(elements);
+  for (let i = 1; i <= userNumber; i += 1) {
+    const newElementDiv = document.createElement('div');
+    newElementDiv.classList.add('div_boxes');
+    newElementDiv.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+    newElementDiv.style.width = '30px';
+    newElementDiv.style.height = '30px';
+
+    console.log(newElementDiv.style.width);
+    console.log(newElementDiv.style.height);
+
+    refs.boxes.append(newElementDiv);
   }
 }
-
-console.log(newElements);
-
-// createBoxes(
-//   refs.number.addEventListener('input', element => {
-//     console.log(element.currentTarget.value);
-//   }),
-// );
-
-createBoxes(11);
-
-// const readyBoxes = createBoxes(
-//   refs.number.addEventListener('input', element => {
-//     console.log(element.currentTarget.value);
-//   }),
-// );
-// console.log(readyBoxes);
-
-refs.boxes.append(...newElements);
-
-// Принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 
 function destroyBoxes() {
   boxes.innerHTML = '';
